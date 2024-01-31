4bac45bf4b027aa7fef376e0a754ba3dc4a6eb9d
karineaiki <ruiz.karine@gmail.com>
2024-01-31 15:18:14 +0100

    correction of canadian currency change

--- src/app.ts
+++ src/app.ts
@@ -6,58 +6,44 @@ form.addEventListener("submit", function (event) {
     result(resultElement);
 });
 
-function result (resultElement: any) {
+function result(resultElement : any) {
     const currency = document.querySelector('#currency') as HTMLSpanElement
     const weight = document.querySelector('#weight-select') as HTMLSelectElement
     const size = document.querySelector('#size-select') as HTMLSelectElement
     const sendTo = document.querySelector('#send-to-select') as HTMLSelectElement
-    
-    
+
+
     let weightResult: number = 0
 
-if (weight.value === "less-1kg" ) {
-    weightResult = 10;
-} else if (weight.value === "between-one-and-three"){
-    weightResult = 20;
-} else {
-    weightResult = 30;
-}; 
+    if (weight.value === "less-1kg") {
+        weightResult = 10;
+    } else if (weight.value === "between-one-and-three") {
+        weightResult = 20;
+    } else {
+        weightResult = 30;
+    };
 
     let sizeResult: number = 0
 
-if (size.value === "more-150cm" ){
-    sizeResult = 5
-};
+    if (size.value === "more-150cm") {
+        sizeResult = 5
+    };
 
-let totalCost: number = weightResult + sizeResult;
+    let totalCost: number = weightResult + sizeResult;
 
-if (sendTo.value === "france"){
-    totalCost
-    currency.textContent = "€"
-} else if (sendTo.value === "canada"){
-    if (totalCost > 12.5 ){
-        totalCost *= 1.15
-        totalCost *= 1,5
-        currency.textContent = "$";
-    } else {
-        totalCost *= 1,5
+    if (sendTo.value === "france") {
+        currency.textContent = "€"
+    } else if (sendTo.value === "canada") {
+        if (totalCost > 12.5) {
+            totalCost *= 1.15
+        }
+        totalCost *= 1.5
         currency.textContent = "$"
-    }
-    
-} else {
-    totalCost *= 130
-    currency.textContent = "¥"
-};
+    } else {
+        totalCost *= 130
+        currency.textContent = "¥"
+    };
 
 
-
-
-resultElement.textContent = totalCost;
+    resultElement.textContent = totalCost;
 }
-
-/*
-euro vers CAD
-1*1,5
-euro vers JPY
-1*130
-*/
